6254d9972c458cb5a9d54958081eb4f5482617ca
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-02-15 19:54:40 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,39 +1,73 @@
+soc_level_state = {[0,20] : 'LOW_SOC_BREACH', [21,24] : 'LOW_SOC_WARNING', [24,75] : 'NORMAL', [76,80] : 'HIGH_SOC_WARNING', [81,100] : 'HIGH_SOC_BREACH' }
+temperature_level_state = {[0,0] : 'LOW_SOC_BREACH', [0,2.25] : 'LOW_TEMPERATURE_WARNING', (2.25,42.75) : 'NORMAL', [42.75,45] : 'HIGH_TEMPERATURE_WARNING', [45,100] : 'HIGH_TEMPERATURE_BREACH'}
+charge_rate_state = {(-100,0] : 'LOW_CHARGE_BREACH', [0,0.2] : 'LOW_CHARGE_WARNING', (0.2,0.8] : 'NORMAL', [76,80] : 'HIGH_CHARGE_WARNING', [81,100] : 'HIGH_CHARGE_BREACH' }
+
 def temperature_check(temperature):
 	if temperature < 0:
 		print('Temperature is out of range!')
+		temperature_tolerance_check
 		low_range()
 		return False
 	elif temperature > 45:
 		print('Temperature is out of range!')
+		temperature_tolerance_check
 		high_range()
 		return False	
 	else :
 		return True
 
+def temperature_tolerance_check(temperature):
+  if ((temperature>=0) and (temperature<=((5/100)*45))):
+    print('Warning: Approaching low temperature')
+  elif ((temperature<=45) and (temperature>=(45-(5/100)*45))):
+    print('Warning: Approaching peak temperature')
+  else:
+    print('Temperature is ok')
+    
 def soc_check(soc):
 	if soc < 20:
 		print('State of Charge is out of range!')
+		soc_tolerance_check(soc)
 		low_range()
 		return False
 	elif soc > 80:
 		print('State of Charge is out of range!')
+		soc_tolerance_check(soc)
 		high_range()
 		return False
 	else:
 		return True
 
+def soc_tolerance_check(soc):
+  if ((soc>=20) and (soc<=(80*5/100+20))):
+    print('Warning: Approaching discharge')
+  elif ((soc>=(80-80*(5/100))) and (soc<=80)):
+    print('Warning: Approaching charge-peak')
+  else:
+    print('SOC Charge is ok')
+    
 def Charge_rate_check(charge_rate):
 	if charge_rate > 0.8:
 		print('Charge rate is out of range!')
+		charge_rate_tolerance_check
 		high_range()
 		return False
 	elif charge_rate < 0:
 		print('Charge rate is out of range!')
+		charge_rate_tolerance_check
 		low_range()
 		return False
 	else:
 		return True
 
+def charge_rate_tolerance_check(charge_rate):
+  if ((charge_rate>=0) and (charge_rate<=((5/100)*0.8))):
+    print('Warning: Approaching low charge rate')
+  elif ((charge_rate<=0.8) and (charge_rate>=(0.8-(5/100)*0.8))):
+    print('Warning: Approaching peak charge rate') 
+  else:
+    print('Charge-rate is ok')
+    
 def battery_is_ok(temperature, soc, charge_rate):
 	return (temperature_check(temperature) and soc_check(soc) and Charge_rate_check(charge_rate))
 
